e1c5ca5c30e59b2db8f16e1a21b0041bda8fc285
mo3az-web <mo3az13579@gmail.com>
2026-08-31 03:31:17 +0300

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
        
       
   ],
-  base: process.env.VITE_BASE_PATH || "/frontend",
+  base: process.env.VITE_BASE_PATH || "/engineering/frontend",
     resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
