852bd152a9f96128d336416ab6801665ea557424
mo3az-web <mo3az13579@gmail.com>
2026-09-03 03:38:09 +0300

    fix vercel routing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,17 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),
-      tailwindcss()
-       
-      
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
-  base: process.env.VITE_BASE_PATH || "",
-    resolve: {
+
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
