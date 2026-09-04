eb75b9ef3c81b90864f14c703642f85b96b1321a
mo3az-web <mo3az13579@gmail.com>
2026-09-04 05:24:58 +0300

    adding features

--- frontend/src/Home/Navbar.tsx
+++ frontend/src/Home/Navbar.tsx
@@ -33,7 +33,7 @@ const services = [
 ]
 
 const navigation = [
-  { name: 'الرئيسية', href: '/' },
+  { name: 'ارئيسية', href: '/' },
   { name: 'عن المكتب', href: '/about' },
   { name: 'مشاريعنا', href: '/projects' },
   { name: 'تواصل معنا', href: '/contact' },
